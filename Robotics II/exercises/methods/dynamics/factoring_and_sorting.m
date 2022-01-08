4e58362dbead1e7a9ba9b3909162c27e159b1f94
David Esteban Imbajoa Ruiz <deivy311@hotmail.com>
2022-01-08 05:39:50 +0100

    Improved dynamics extractor function performance

--- Robotics II/exercises/methods/dynamics/factoring_and_sorting.m
+++ Robotics II/exercises/methods/dynamics/factoring_and_sorting.m
@@ -2,12 +2,13 @@
 %email:     imbajoaruiz.1922212@studenti.uniroma1.it
 %functiontion to extract numeric factors and for sorting
 function [terms_to_process_C,symbols_signs]=factoring_and_sorting(terms_to_process,extract_num)
-    temp_symbolic_term=sym('temp_symbolic_term_mul','real');
+%     temp_symbolic_term=sym('temp_symbolic_term_mul','real');
     symbols_signs=ones(size(terms_to_process));
     
     if(extract_num)
         for  i=1:length(terms_to_process)
-            temporal_syms_childs=cell2sym(children(terms_to_process(i)*temp_symbolic_term));
+            %temporal_syms_childs=cell2sym(children(terms_to_process(i)*temp_symbolic_term));
+            temporal_syms_childs=factor(terms_to_process(i));
             temp_values_ind=isSymType(temporal_syms_childs,'real');   
             symbols_signs(i)=prod(temporal_syms_childs(temp_values_ind));
             terms_to_process(i)=simplify(terms_to_process(i)/symbols_signs(i));
